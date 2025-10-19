8c96f3b552c11a40a88767b62777a922948303b8
Arm91 <Armen-006@mail.ru>
2025-10-19 21:20:21 +0400

    HW-20 Task 1 and Task 2

--- src/ui/tests/demo-login.form.spec.ts
+++ src/ui/tests/demo-login.form.spec.ts
@@ -36,14 +36,13 @@ test.describe("[REGISTER PAGE]", { tag: "@smoke" }, () => {
       const usernameInput = page.locator("#userNameOnRegister");
       const passwordInput = page.locator("#passwordOnRegister");
       const registerButton = page.locator("#register");
+      const successText = page.locator("text=Successfully registered! Please, click Back to return on login page");
 
       await usernameInput.fill(validCredentials.username);
       await passwordInput.fill(validCredentials.password);
       await registerButton.click();
 
-      await expect(
-        page.locator("text=Successfully registered! Please, click Back to return on login page"),
-      ).toBeVisible();
+      await expect(successText).toBeVisible();
     },
   );
 
@@ -73,13 +72,12 @@ test.describe("[REGISTER PAGE]", { tag: "@smoke" }, () => {
     const loginUsernameInput = page.locator("#userName");
     const loginPasswordInput = page.locator("#password");
     const submitButton = page.locator("#submit");
+    const successText = page.locator("text=Successfully registered! Please, click Back to return on login page");
 
     await usernameInput.fill(validCredentials.username);
     await passwordInput.fill(validCredentials.password);
     await registerButton.click();
-    await expect(
-      page.locator("text=Successfully registered! Please, click Back to return on login page"),
-    ).toBeVisible();
+    await expect(successText).toBeVisible();
     await expect(page.locator("#backOnRegister")).toBeVisible();
     await backButton.click();
 
@@ -89,3 +87,59 @@ test.describe("[REGISTER PAGE]", { tag: "@smoke" }, () => {
     await expect(page.locator(`text=Hello, ${validCredentials.username}`)).toBeVisible();
   });
 });
+
+/*
+Homework 20 Task 2
+Разработать тест со следующими шагами:
+  - открыть https://anatoly-karpovich.github.io/demo-login-form/
+  - Засунуть в localStorage браузера данные test@gmail.com / SecretPw123!@# для логина на сайт
+  - Залогиниться с данными что вы вставили в localStorage
+  - Завалидировать успешный логин
+
+  Рекоммендации:
+  - Для доступа к localStorage используйте https://playwright.dev/docs/evaluating
+*/
+
+test.describe("[LOGIN PAGE] Homework 20 Task 2", () => {
+  const credentials = {
+    name: "test@gmail.com",
+    password: "SecretPw123!@#",
+  };
+
+  test.beforeEach(async ({ page }) => {
+    const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+    await page.goto(url);
+  });
+
+  test("Local storage test", async ({ page }) => {
+    const loginUsernameInput = page.locator("#userName");
+    const loginPasswordInput = page.locator("#password");
+    const submitButton = page.locator("#submit");
+
+    await page.evaluate((data) => {
+      localStorage.setItem(data.name, JSON.stringify(data));
+    }, credentials);
+
+    await page.reload();
+
+    await loginUsernameInput.fill(credentials.name);
+    await loginPasswordInput.fill(credentials.password);
+    await submitButton.click();
+    await expect(page.locator(`text=Hello, ${credentials.name}`)).toBeVisible();
+  });
+});
+
+/*
+Homework 21 Task 1
+Создать тест сьют используя DDT подход с негативными тест-кейсами по регистрации на сайте
+https://anatoly-karpovich.github.io/demo-login-form/
+
+Требования:
+Страница регистрации:
+  Username: обязательное, от 3 до 40 символов включительно, запрещены префиксные/постфиксные пробелы, как и имя состоящее из одних пробелов
+  Password: обязательное, от 8 до 20 символов включительно, необходима хотя бы одна буква в верхнем и нижнем регистрах, пароль из одних пробелов запрещен
+
+Страница логина:
+  Username: обязательное
+  Password: обязательное
+*/
